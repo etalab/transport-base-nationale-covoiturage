e0f195163bebfa5923ef1ec4cb564b4bb62cd261
Antoine Augusti <antoine.augusti@gmail.com>
2023-12-20 15:22:21 +0100

    Refactor

--- test.py
+++ test.py
@@ -11,35 +11,36 @@ def dataset_api_url(dataset_url):
     slug = dataset_url.replace("https://www.data.gouv.fr/fr/datasets/", "")
     return f"https://www.data.gouv.fr/api/1/datasets/{slug}"
 
-def ressource_validation(ressource_url):
-        params = {"url":ressource_url , "schema": URL_SCHEMA}
-        response_validata = requests.get("https://api.validata.etalab.studio/validate", params=params)
-        response_validata.raise_for_status()
-        return response_validata.json()["report"].get("valid")
+
+def resource_is_valid(resource_url):
+    params = {"url": resource_url, "schema": URL_SCHEMA}
+    validata_response = requests.get("https://api.validata.etalab.studio/validate", params=params)
+    validata_response.raise_for_status()
+    return validata_response.json()["report"].get("valid")
+
 
 with open(FILENAME) as f:
     rows = [r for r in csv.DictReader(f)]
     errors = []
-    row_number = 1
-    for row in rows:
-        row_number += 1
+    for row_number, row in enumerate(rows, 1):
         dataset_url = row["dataset_url"]
         response = requests.get(dataset_api_url(dataset_url))
         response.raise_for_status()
-        haveSchema = False
+        hasSchema = False
 
         for r in response.json()["resources"]:
             if r["schema"].get("name") == TARGET_SCHEMA:
-                haveSchema = True
-                if not ressource_validation(r["url"]):
-                    errors.append(f"LIGNE {row_number} - {dataset_url} : la ressource {r['title']} n'est pas conforme au schéma")
-        
-        if not haveSchema:
+                hasSchema = True
+                if not resource_is_valid(r["url"]):
+                    errors.append(
+                        f"LIGNE {row_number} - {dataset_url} : la ressource {r['title']} n'est pas conforme au schéma"
+                    )
+
+        if not hasSchema:
             errors.append(f"LIGNE {row_number} - {dataset_url} : aucune ressource avec le schéma")
-        
 
     if len(errors) > 0:
-        raise ValueError('\n'.join(errors))
+        raise ValueError("\n".join(errors))
 
     urls = [r["dataset_url"] for r in rows]
     duplicates = set([u for u in urls if urls.count(u) > 1])
