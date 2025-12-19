16b491140f630524424ff60c79c3a96838a62095
Antoine Augusti <antoine.augusti@gmail.com>
2025-12-19 14:44:17 +0100

    Update tests

--- test.exs
+++ test.exs
@@ -1,4 +1,8 @@
-Mix.install([{:csv, "~> 3.2"}])
+Mix.install([
+	{:csv, "~> 3.2"},
+	{:httpoison, "~> 2.0"},
+	{:jason, "~> 1.4"}
+])
 
 require Logger
 
@@ -7,4 +11,14 @@ File.stream!("bnlc-.csv")
 |> Enum.to_list()
 |> to_string()
 
-Logger.info("File is okay")
+Logger.info("Can decode the file")
+
+github_url = "https://raw.githubusercontent.com/etalab/transport-base-nationale-covoiturage/refs/heads/main/bnlc-.csv"
+schema_url = "https://schema.data.gouv.fr/schemas/etalab/schema-lieux-covoiturage/latest/schema.json"
+
+%{"report" => %{"valid" => true}} = return =
+	HTTPoison.get!("https://api.validata.etalab.studio/validate?url=#{github_url}&schema=#{schema_url}")
+	|> Map.fetch!(:body)
+	|> Jason.decode!()
+Logger.debug(return)
+Logger.info("File is valid")
